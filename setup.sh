d9217096d6577a86ea93668c479ba206153bb90f
luhurfatah <luhurfatah@gmail.com>
2022-11-08 12:43:15 +0700

    cek

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ docker run -d --name influxdb -p 8086:8086 influxdb:2.3
 docker build simpleservice/
 
 influxdb_ip = docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' influxdb
+simpleservice_ip = docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' simpleservice
+docker build nginx/
